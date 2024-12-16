5c417d9c724c23c887afdba2a3ffb41c1fb99a26
Kevin Dharmawan <kevindharmawan2002@gmail.com>
2024-12-16 15:27:49 -0500

    Trying to fix github workflows 4

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  basePath: "/personal-website",
+  output: "export",
 };
 
 export default nextConfig;
